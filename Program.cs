d3d5e2484337e594ee7ed7a6cbccd41238967bca
kUcLis <ulis89@gmail.com>
2023-01-25 10:29:52 +0100

    Final program showing first 20 numbers of Fibonnaci Sequence

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var numbers = new List<int> {1,1};
+            
+            for(int i = 0; i < 20; i++)
+            {
+                if(i != 0)
+                {
+                    var previous = numbers[i - 1];
+                    numbers.Add(previous + numbers[i]);
+                }
+                Console.WriteLine(numbers[i]);
+            }
+
         }
     }
 }
